c61aee3ac92f4bb8df55e2c74c7b629ca2ba2718
visor517 <visor517@yandex.ru>
2022-06-09 17:05:11 +0300

    added Product

--- storeapp/models.py
+++ storeapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    arrived_at = models.DateField()
+    price = models.IntegerField()
+    unit = models.CharField(max_length=32)
+    arrived_from = models.CharField(max_length=64)
