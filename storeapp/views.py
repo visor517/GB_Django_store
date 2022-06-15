d8a0d2712eae07ae041bd56db13481dff355a7b3
visor517 <visor517@yandex.ru>
2022-06-16 00:48:24 +0300

    menu is working

--- storeapp/views.py
+++ storeapp/views.py
@@ -6,8 +6,12 @@ from .models import Category, Product
 
 class ProductListView(ListView):
     model = Product
-    queryset = Product.objects.prefetch_related('categories').all()
     template_name = 'index.html'
     extra_context = {
         'categories': Category.objects.all()
     }
+
+    def get_queryset(self):
+        if 'pk' in self.kwargs:
+            return Product.objects.prefetch_related('categories').filter(categories=self.kwargs['pk'])
+        return Product.objects.prefetch_related('categories').all()
